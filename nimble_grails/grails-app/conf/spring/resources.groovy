b4b8ba035da30c0d820ed9c326b744245835cad4
Alan <alan.zuppicich@xtra.co.nz>
2018-01-12 09:54:17 +1300

    leave DateEditorRegistrar out for now...

--- nimble_grails/grails-app/conf/spring/resources.groovy
+++ nimble_grails/grails-app/conf/spring/resources.groovy
@@ -1,12 +1,5 @@
-/*
-
-aNimble Platform: Application Lifecycle Management Tool
-Copyright (C) 2010-2012 ideaStub, LLC
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
+// Place your Spring DSL code here
+package spring
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
