2d26af26dd83b69b19920be0563232052a3004c8
Alan <alan.zuppicich@xtra.co.nz>
2018-01-12 12:04:58 +1300

    fix resources.groovy, still hide DateEditorRegistrar for now...

--- nimble_grails/grails-app/conf/spring/resources.groovy
+++ nimble_grails/grails-app/conf/spring/resources.groovy
@@ -1,5 +1,12 @@
-// Place your Spring DSL code here
-package spring
+/*
+
+aNimble Platform: Application Lifecycle Management Tool
+Copyright (C) 2010-2012 ideaStub, LLC
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -12,7 +19,9 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 */
 
+package spring
+
 // Place your Spring DSL code here
 beans = {
-	    customPropertyEditorRegistrar(com.nimble.domain.DateEditorRegistrar)
+	    //customPropertyEditorRegistrar(com.nimble.domain.DateEditorRegistrar)
 }
