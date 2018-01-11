ba1b482e56edd8cd119dc5459921e7200f00f4d2
Alan <alan.zuppicich@xtra.co.nz>
2018-01-12 09:59:57 +1300

    Move Bootstrap.groovy as part of Config migration.
    conf/Config.groovy already renamed to conf/application grovy,
    conf/DataSource.groovy already merged into conf/application.groovy

--- nimble_grails/grails-app/init/nimble_grails/BootStrap.groovy
+++ nimble_grails/grails-app/init/nimble_grails/BootStrap.groovy
@@ -1,8 +1,34 @@
 package nimble_grails
 
+import com.nimble.domain.view.ApplicationControl
+import com.nimble.domain.reference.*
+import com.nimble.utils.view.ViewHelper
+import com.nimble.utils.domain.ReferenceHelper
+import com.nimble.utils.file.FileHelper
+import com.nimble.controller.view.ApplicationControlController
+import com.nimble.controller.reference.ReferenceController
+import com.nimble.controller.security.ApplicationSettingController
+
 class BootStrap {
 
     def init = { servletContext ->
+        //println '****************** Bootstap begin'
+
+        // cache app control data
+        def appController = new ApplicationControlController()
+        appController.loadCache(servletContext)
+
+        // cache ref data
+        def refController = new ReferenceController()
+        refController.loadCache(servletContext)
+
+        // cache settings data
+        def settingController = new ApplicationSettingController()
+        settingController.loadCache(servletContext)
+
+        //println '****************** Bootstap end'
+
+
     }
     def destroy = {
     }
